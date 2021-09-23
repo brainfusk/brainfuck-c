2781455b951faa3e72ac175db3271cbf05b23751
techzealot <techzealot@foxmail.com>
2021-09-23 11:09:30 +0800

    polish brainfuck interpreter version

--- brainfuck.c
+++ brainfuck.c
@@ -5,26 +5,31 @@
 #include <stdio.h>
 #include <sys/fcntl.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
 #define MAX_STACK 1000
 
+#define MAX_PROGRAM_SIZE 50000
+
+#define die(...) do { fprintf(stderr, __VA_ARGS__); fputc('\n', stderr); exit(EXIT_FAILURE); } while (0)
+
 void prepareJumpTable(char *program, size_t program_size, char **jumpTable) {
     size_t stack[MAX_STACK] = {0};
-    size_t *sp = stack;
+    size_t sp = 0;
     char *ptr = program;
     while (ptr < (program + program_size)) {
         if (*ptr == '[') {
-            *sp = ptr - program;
-            ++sp;
+            stack[sp++] = ptr - program;
         }
         if (*ptr == ']') {
-            jumpTable[ptr - program] = program + *(sp - 1);
-            jumpTable[*(sp - 1)] = ptr;
-            sp--;
+            size_t loop_start = stack[--sp];
+            jumpTable[ptr - program] = program + loop_start;
+            jumpTable[loop_start] = ptr;
         }
         ptr++;
     }
-    if (sp != stack) {
+    if (sp != 0) {
         perror("stack not balanced");
     }
 }
@@ -34,20 +39,21 @@ int main(int argc, char *argv[]) {
         perror("must input a bf file");
         exit(-1);
     }
-    char program[50000]={0}, memory[30000]={0};
-    //存储跳转信息
-    char *jumpTable[50000]={0};
-    char *ip = program;
-    char *ptr = memory;
+    char memory[30000] = {0};
     char *filename = argv[1];
     FILE *file = fopen(filename, "rb");
     if (file == NULL) {
         printf("error open file %s", filename);
         exit(-1);
     }
+    char *program = NULL;
     size_t read;
     size_t program_size = 0;
     do {
+        program = program ? realloc(program, program_size + 1024) : malloc(1024);
+        if (program == NULL) {
+            die("Error allocating memory: %s.", strerror(errno));
+        }
         read = fread(program + program_size, 1, 1024, file);
         program_size += read;
     } while (read == 1024);
@@ -56,6 +62,10 @@ int main(int argc, char *argv[]) {
         exit(-1);
     }
     fclose(file);
+    char *ip = program;
+    char *ptr = memory;
+    //存储跳转信息
+    char *jumpTable[MAX_PROGRAM_SIZE] = {0};
     prepareJumpTable(program, program_size, jumpTable);
     while (ip < (program + program_size)) {
         switch (*ip) {
